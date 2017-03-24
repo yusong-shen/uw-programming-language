be117a48a24c07776fb0282e1fab457331048464
yusong-shen <yusongtju@gmail.com>
2017-03-24 16:09:52 -0500

    enhancement1 : rotate 180 degree

--- hw6/hw6assignment.rb
+++ hw6/hw6assignment.rb
@@ -13,12 +13,27 @@ end
 class MyBoard < Board
   # your enhancements here
 
+  # rotates the current piece 180 degree
+  def rotate_180
+    rotate_clockwise
+    rotate_clockwise
+  end
 end
 
 class MyTetris < Tetris
   # your enhancements here
-  def method_name
-      
+  # override to use MyBoard
+  def set_board
+    @canvas = TetrisCanvas.new
+    @board = MyBoard.new(self)
+    @canvas.place(@board.block_size * @board.num_rows + 3,
+                  @board.block_size * @board.num_columns + 6, 24, 80)
+    @board.draw
+  end
+
+  def key_bindings
+      super
+      @root.bind('u', proc {@board.rotate_180})
   end
 end
 
